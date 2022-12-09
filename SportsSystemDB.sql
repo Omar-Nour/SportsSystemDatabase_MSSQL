0160f7dc9f2eeb1bcffa8f43172c50c7f398e365
Omar-Nour <omar.syd.n@gmail.com>
2022-12-09 16:39:25 +0200

    Fixed Primary key to match the schema, Fixed clearAllTables and dropAllTables

--- SportsSystemDB.sql
+++ SportsSystemDB.sql
@@ -12,45 +12,46 @@ AS
 
 	CREATE TABLE SystemAdmin (
 		id int IDENTITY,
-		username VARCHAR(20),
+		username VARCHAR(20) UNIQUE,
 		name VARCHAR(20),
-		CONSTRAINT SysAdminPK PRIMARY KEY (id,username),
+		CONSTRAINT SysAdminPK PRIMARY KEY (id),
 		FOREIGN KEY (username) REFERENCES SystemUser
+
 	);
 
 	CREATE TABLE StadiumManager(
 		id int IDENTITY,
-		username VARCHAR(20),
+		username VARCHAR(20) UNIQUE,
 		name VARCHAR(20),
-		CONSTRAINT StadManPK PRIMARY KEY (id,username),
+		CONSTRAINT StadManPK PRIMARY KEY (id),
 		FOREIGN KEY (username) REFERENCES SystemUser
 	);
 
 	CREATE TABLE ClubRepresentative(
 		id int IDENTITY,
-		username VARCHAR(20),
+		username VARCHAR(20) UNIQUE,
 		name VARCHAR(20),
-		CONSTRAINT ClubRepPK PRIMARY KEY (id,username),
+		CONSTRAINT ClubRepPK PRIMARY KEY (id),
 		FOREIGN KEY (username) REFERENCES SystemUser
 	);
 
 	CREATE TABLE Fan(
-		username VARCHAR(20),
+		username VARCHAR(20) UNIQUE,
 		NationalID VARCHAR(20),
 		PhoneNo VARCHAR(20),
 		Address VARCHAR(20),
 		name VARCHAR(20),
 		status bit,
 		BirthDate date,
-		CONSTRAINT FanPK PRIMARY KEY (NationalID,username),
+		CONSTRAINT FanPK PRIMARY KEY (NationalID),
 		FOREIGN KEY (username) REFERENCES SystemUser
 	);
 
 	CREATE TABLE SportsAssociationManager(
 		id int IDENTITY,
-		username VARCHAR(20),
+		username VARCHAR(20) UNIQUE,
 		name VARCHAR(20),
-		CONSTRAINT SPAM_PK PRIMARY KEY (id,username),
+		CONSTRAINT SPAM_PK PRIMARY KEY (id),
 		FOREIGN KEY (username) REFERENCES SystemUser
 	);
 
@@ -62,8 +63,8 @@ AS
 		status bit,
 		StadiumManagerID int,
 		StadiumManagerUserName VARCHAR(20),
-		FOREIGN KEY (StadiumManagerID,StadiumManagerUserName) 
-			REFERENCES StadiumManager(id,username)
+		FOREIGN KEY (StadiumManagerID) 
+			REFERENCES StadiumManager(id)
 	);
 
 	CREATE TABLE Club(
@@ -72,8 +73,8 @@ AS
 		location VARCHAR(20),
 		ClubRepresentativeID int,
 		ClubRepresentativeUserName VARCHAR(20),
-		FOREIGN KEY (ClubRepresentativeID,ClubRepresentativeUserName) 
-			REFERENCES ClubRepresentative(id,username)
+		FOREIGN KEY (ClubRepresentativeID) 
+			REFERENCES ClubRepresentative(id)
 	);
 
 	CREATE TABLE Match(
@@ -91,8 +92,8 @@ AS
 		FanUserName VARCHAR(20), 
 		FanNationalID VARCHAR(20), 
 		MatchID int FOREIGN KEY REFERENCES Match,
-		FOREIGN KEY (FanNationalID,FanUserName) 
-			REFERENCES Fan(NationalID,username)
+		FOREIGN KEY (FanNationalID) 
+			REFERENCES Fan(NationalID)
 	);
 
 	CREATE TABLE HostRequest(
@@ -103,10 +104,10 @@ AS
 		StadiumManagerUserName VARCHAR(20),
 		ClubRepresentativeID int,
 		ClubRepresentativeUserName VARCHAR(20),
-		FOREIGN KEY (ClubRepresentativeID,ClubRepresentativeUserName) 
-			REFERENCES ClubRepresentative(id,username),
-		FOREIGN KEY (StadiumManagerID,StadiumManagerUserName) 
-			REFERENCES StadiumManager(id,username)
+		FOREIGN KEY (ClubRepresentativeID) 
+			REFERENCES ClubRepresentative(id),
+		FOREIGN KEY (StadiumManagerID) 
+			REFERENCES StadiumManager(id)
 	);
 GO
 
@@ -114,17 +115,17 @@ GO
 --Procedure that drops all of the created tables 
 CREATE PROCEDURE dropAllTables 
 AS
-	DROP TABLE SystemUser;
+	DROP TABLE Ticket;
+	DROP TABLE HostRequest;
+	DROP TABLE Match;
+	DROP TABLE Stadium;
+	DROP TABLE Club;
 	DROP TABLE SystemAdmin;
 	DROP TABLE StadiumManager;
 	DROP TABLE ClubRepresentative;
 	DROP TABLE Fan;
 	DROP TABLE SportsAssociationManager;
-	DROP TABLE Stadium;
-	DROP TABLE Club;
-	DROP TABLE Match;
-	DROP TABLE Ticket;
-	DROP TABLE HostRequest;
+	DROP TABLE SystemUser;
 GO
 
 GO
@@ -150,17 +151,17 @@ GO
 --Procedure that clears the rows of all the tables
 CREATE PROCEDURE clearAllTables
 AS
-	TRUNCATE TABLE SystemUser;
-	TRUNCATE TABLE SystemAdmin;
-	TRUNCATE TABLE StadiumManager;
-	TRUNCATE TABLE ClubRepresentative;
-	TRUNCATE TABLE Fan;
-	TRUNCATE TABLE SportsAssociationManager;
-	TRUNCATE TABLE Stadium;
-	TRUNCATE TABLE Club;
-	TRUNCATE TABLE Match;
-	TRUNCATE TABLE Ticket;
-	TRUNCATE TABLE HostRequest;
+	DELETE FROM Ticket;
+	DELETE FROM HostRequest;
+	DELETE FROM Match;
+	DELETE FROM Stadium;
+	DELETE FROM Club;
+	DELETE FROM SystemAdmin;
+	DELETE FROM StadiumManager;
+	DELETE FROM ClubRepresentative;
+	DELETE FROM Fan;
+	DELETE FROM SportsAssociationManager;
+	DELETE FROM SystemUser;
 GO
 
 GO
@@ -179,7 +180,7 @@ CREATE VIEW allClubRepresentatives AS
 	SELECT R.username AS RepUserName,R.name AS RepName,C.name AS ClubName
 		FROM ClubRepresentative AS R,Club AS C
 		WHERE R.id = C.ClubRepresentativeID 
-			AND R.username = C.ClubRepresentativeUserName;
+			
 GO
 
 GO
@@ -190,7 +191,7 @@ CREATE VIEW allStadiumManagers AS
 	SELECT M.username AS StadManUserName,M.name AS StadManName,S.name AS StadiumName
 		FROM StadiumManager AS M,Stadium AS S
 		WHERE M.id = S.StadiumManagerID 
-			AND M.username = S.StadiumManagerUserName;
+			
 GO
 
 GO
