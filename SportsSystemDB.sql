c89e404b16db4f8f4134ffdb3f51ebd148f596e3
Omar-Nour <omar.syd.n@gmail.com>
2022-12-13 22:10:25 +0200

    Implemneted XXI to XXIX

--- SportsSystemDB.sql
+++ SportsSystemDB.sql
@@ -38,7 +38,7 @@ AS
 	CREATE TABLE Fan(
 		username VARCHAR(20) UNIQUE,
 		NationalID VARCHAR(20),
-		PhoneNo VARCHAR(20),
+		PhoneNo int,
 		Address VARCHAR(20),
 		name VARCHAR(20),
 		status bit,
@@ -251,3 +251,139 @@ CREATE VIEW allRequests AS
 			WHERE SM.id = H.StadiumManagerID 
 			AND CR.id = H.ClubRepresentativeID;
 GO
+
+------- FROM XXI TO XXX -------
+
+-- XXI
+CREATE PROCEDURE addFan
+@name varchar(20),
+@username varchar(20),
+@password varchar(20),
+@nid varchar(20),
+@bd datetime,
+@address varchar(20),
+@phone_num int
+AS
+INSERT INTO SystemUser VALUES (@username, @password);
+INSERT INTO Fan VALUES (@username, @nid, @phone_num, @address, @name, 1, @bd);
+GO
+
+-- XXII
+CREATE FUNCTION [upcomingMatchesOfClub]
+(@club_name varchar(20))
+RETURNS TABLE AS 
+	RETURN SELECT C1.name as club, C2.name as competing_club, M.StartTime , S.name as stadium_name
+			FROM Club C1, Club C2, Match M, Stadium S
+			WHERE C1.name = @club_name AND ((C1.id = M.HostClubID AND C2.id = M.GuestClubID) OR 
+			(C2.id = M.HostClubID AND C1.id = M.GuestClubID)) AND C1.name <> C2.name AND M.StadiumID = S.id
+			AND CURRENT_TIMESTAMP < M.StartTime;
+GO
+
+-- XXIII
+CREATE FUNCTION [availableMatchesToAttend]
+(@date datetime)
+RETURNS TABLE AS 
+	RETURN SELECT H.name as host, G.name as guest, M.StartTime , S.name as stadium_name
+			FROM Club H, Club G, Match M, Stadium S
+			WHERE M.StartTime >= @date AND (H.id = M.HostClubID AND G.id = M.GuestClubID)
+			AND EXISTS (SELECT * FROM Ticket T WHERE T.MatchID = M.id AND T.status = 1);
+GO
+
+-- XXIV
+CREATE PROCEDURE purchaseTicket
+@host_name varchar(20),
+@guest_name varchar(20),
+@nid varchar(20),
+@start_time datetime
+AS
+	DECLARE @ticket_id int;
+	DECLARE @fan_user_name varchar(20);
+	DECLARE @match_id int;
+	SET @match_id = (SELECT M.id FROM Club H, Club G, Match M
+					 WHERE H.id = M.HostClubID AND G.id = M.GuestClubID AND
+					  M.StartTime = @start_time AND  H.name = @host_name
+					  AND G.name = @guest_name
+	);
+	SET @ticket_id = (SELECT MIN(T.id) FROM Ticket T, Match M
+					  WHERE M.id = T.MatchID AND T.status = 1 
+	);
+	SET @fan_user_name = (SELECT username FROM Fan WHERE NationalID = @nid);
+
+	UPDATE Ticket SET FanUserName = @fan_user_name, 
+				      FanNationalID = @nid,
+					  status = 0
+				  WHERE id = @ticket_id;
+GO
+
+-- XXV
+CREATE PROCEDURE updateMatchHost
+@host_name varchar(20),
+@guest_name varchar(20),
+@start_time datetime
+AS
+	DECLARE @host_id int;
+	DECLARE @guest_id int;
+	DECLARE @match_id int;
+	SELECT @match_id = M.id, @host_id = M.HostClubID, @guest_id = M.GuestClubID 
+			FROM Club H, Club G, Match M
+			WHERE H.id = M.HostClubID AND G.id = M.GuestClubID AND
+			M.StartTime = @start_time AND  H.name = @host_name
+			AND G.name = @guest_name;
+	
+	UPDATE Match SET HostClubID = @guest_id, 
+				     GuestClubID = @host_id
+				  WHERE id = @match_id;
+GO
+
+-- XXVI
+CREATE VIEW matchesPerTeam AS
+	SELECT C.name as club_name, COUNT(M.id) as matchs_played FROM Club C, Match M
+	WHERE (C.id = M.HostClubID OR C.id = M.GuestClubID) 
+	AND M.EndTime <= CURRENT_TIMESTAMP
+	GROUP BY C.name;
+GO
+
+-- XXVII
+CREATE VIEW clubsNeverMatched AS
+	SELECT C1.name as club1, C2.name as club2
+	FROM Club C1, Club C2
+	WHERE C1.id < C2.id 
+	AND NOT EXISTS (SELECT * FROM Match M 
+					WHERE  (M.HostClubID = C1.id AND M.GuestClubID = C2.id)
+					OR (M.HostClubID = C2.id AND M.GuestClubID = C1.id) AND
+					M.StartTime > CURRENT_TIMESTAMP
+	);
+GO
+
+-- XXVIII
+CREATE FUNCTION [clubsNeverPlayed]
+(@club_name varchar(20))
+RETURNS TABLE
+AS
+	RETURN (SELECT C.name FROM clubsNeverMatched V, Club C
+			WHERE (C.name = V.club2 and V.club1 = @club_name)
+			OR (C.name = V.club1 and V.club2 = @club_name)
+	);
+GO
+
+-- XXIX
+CREATE FUNCTION [matchWithHighestAttendance]()
+RETURNS TABLE
+AS
+	RETURN (SELECT H.name as host_club, G.name as guest_club 
+			FROM Match M, Club H, Club G, Ticket T
+			WHERE (H.id = M.HostClubID AND G.id = M.GuestClubID)
+			AND T.MatchID = M.id AND T.status = 0
+			GROUP BY H.name, G.name
+			HAVING COUNT(T.id) >= ALL (SELECT COUNT(T2.id)
+								 FROM Ticket T2
+								 WHERE T2.status = 0
+								 GROUP BY T2.MatchID)
+	);
+GO
+
+
+
+
+
+----------------------------
