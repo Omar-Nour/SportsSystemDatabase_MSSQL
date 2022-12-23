8d9484e9f11ff6ae25c82181bd77ac59ed3f5fd2
youssefalaa398 <119411027+youssefalaa398@users.noreply.github.com>
2022-12-23 21:18:00 +0200

    added stadium manager

---
+++ SportSys/Stadium manager.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SportSys
+{
+    public partial class Stadium_manager : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
