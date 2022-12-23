42255fe9927045b3614c642545d34587ef041bee
Mohamed Shamekh <shamekhjr@gmail.com>
2022-12-24 00:36:00 +0200

    Created fan page and added label that displays username

---
+++ SportSys/Fan.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SportSys
+{
+    public partial class Fan : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Label1.Text = "username: " + (string)Session["username"];
+        }
+    }
+
+
+}
