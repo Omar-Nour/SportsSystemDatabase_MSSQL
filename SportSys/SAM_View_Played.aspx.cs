daf88ef47ec505ad9e8c779526b368762a10a5be
Ali Eldin <alieldin82@gmail.com>
2022-12-28 15:55:50 +0200

    miscellaneous

--- SportSys/SAM_View_Played.aspx.cs
+++ SportSys/SAM_View_Played.aspx.cs
@@ -10,13 +10,13 @@ namespace SportSys
         protected void Page_Load(object sender, EventArgs e)
         {
 #pragma warning disable CS0252 // Possible unintended reference comparison; left hand side needs cast
-           /* if (Session["username"] == null || Session["type"] != "sam")
+            if (Session["username"] == null || Session["type"] != "sam")
             {
                 Response.Redirect("Login.aspx");
                 return;
             }
 #pragma warning restore CS0252 // Possible unintended reference comparison; left hand side needs cast
-            Played_Sys_User_name.Text = Session["username"].ToString();*/
+            Played_Sys_User_name.Text = Session["username"].ToString();
 
             Int64 total = 0;
 
